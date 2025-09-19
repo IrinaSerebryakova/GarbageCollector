d846d04e216509665edfcfce8e7861defe2df4db
Серебрякова Ирина <i.serebryakova@mail.astondevs.ru>
2025-09-19 10:05:18 +0300

    создание объектов в Eden. heap увеличить до 2048 (или уменьшить до 512, чтобы поймать outOfMemoryError

--- src/MemoryDemoApplication.java
+++ src/MemoryDemoApplication.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /*
@@ -12,9 +14,30 @@ import java.util.concurrent.TimeUnit;
 public class MemoryDemoApplication {
 
 	public static void main(String[] args) throws InterruptedException {
+
+		// Выделение большого массива, как в исходном примере
 		var array = new byte[1_000_000_000];
-		for (int i = 0; i < 1_000_000_000; i++) {
-			array[i] = (byte) i;
+
+        // Список для хранения ссылок на объекты, которые будут перемещены в старшее поколение
+		List<byte[]> retainedObjects = new ArrayList<>();
+
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			// Запись в большой массив
+			array[i] = 1;
+
+			// Создание нового объекта (1KB массива) в Eden
+			byte[] edenObject = new byte[1024]; // 1KB
+
+			// По желанию можно удерживать некоторые объекты для предотвращения их сборки
+			if (i % 50_000 == 0) {
+				retainedObjects.add(edenObject); // Удерживаем объект
+				// Ограничиваем размер списка, чтобы не заполнить старшее поколение
+				if (retainedObjects.size() > 1000) {
+					retainedObjects.remove(0);
+				}
+			}
+
+			// Каждые 10_000 итераций делаем паузу
 			if (i % 10_000 == 0) {
 				System.out.println("Пауза на 1 секунду");
 				Thread.sleep(TimeUnit.SECONDS.toMillis(1));
